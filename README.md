c43ccb9399e2c28efd11433d384d3c90d719f958
vespermystcl <mgmt@mystcl.xyz>
2024-05-24 15:21:25 +0400

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MYST Token Public Repo
+
+This repo was created to provide a public source of for the $MYST token.
+
+It is a simplified version of the core private repo for MYSTCL project contracts, however it is identical to the live contract as can be confirmed onchain.
---
+++ contracts/myst/Structs.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+library SharedStructs {
+  // Events
+  event ERC20Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 amount
+  );
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 amount
+  );
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 indexed id
+  );
+  event ERC721Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 indexed id
+  );
+  event ApprovalForAll(
+    address indexed owner,
+    address indexed operator,
+    bool approved
+  );
+
+  // Errors
+  error NotFound();
+  error AlreadyExists();
+  error InvalidRecipient();
+  error InvalidSender();
+  error UnsafeRecipient();
+  error InvalidId();
+  error IdNotAssigned();
+  error PoolIsEmpty();
+  error InvalidSetWhitelistCondition();
+}
